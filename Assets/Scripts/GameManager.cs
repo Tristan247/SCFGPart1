f1da51a0cd25d013f874188373602bd4a69c66a8
Tristan247 <56597679+Tristan247@users.noreply.github.com>
2021-02-01 20:10:26 +0100

    Task 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    gridCreator grid;
+
+    void Start()
+    {
+        grid = GetComponent<gridCreator>();
+        for (int i = 0; i < 5; i++)
+        {
+            AddObs();
+        }
+        Scan();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scan()
+    {
+        AstarPath.active.Scan();
+    }
+    public void AddObs()
+    {
+        Instantiate(Resources.Load<GameObject>("PreFabs/Obstacle"), new Vector3(Random.Range(0, grid.width), Random.Range(0, grid.height)), Quaternion.identity);
+    }
+    public void AddAI()
+    {
+        Instantiate(Resources.Load<GameObject>("PreFabs/AI"), new Vector3(Random.Range(0, grid.width), Random.Range(0, grid.height)), Quaternion.identity);
+    }
+    public void StartGame()
+    {
+
+    }
+}
---
+++ Assets/gridCreator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridCreator : MonoBehaviour
+{
+    public int width;
+    public int height;
+    Camera cam;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                Instantiate(Resources.Load<GameObject>("PreFabs/Cube"), new Vector3(i, j), Quaternion.identity);
+            }
+        }
+        cam.orthographicSize = (width/2) + 10;
+        cam.transform.position = new Vector3(width/2, height/2, -10);
+    }
+}
